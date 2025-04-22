b3da28c8a895b74a7f71013a264bc7a0c0bcbb2b
jgfn1 <jgfn1@cin.ufpe.br>
2025-04-22 14:07:39 -0300

    Update backend to integrate Sequelize with MySQL; add dotenv for environment variable management, and update package dependencies in package.json and package-lock.json.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,32 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProdutosModule } from './produtos/produtos.module';
 import { PedidosModule } from './pedidos/pedidos.module';
 import { UnidadesModule } from './unidades/unidades.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [AuthModule, ProdutosModule, PedidosModule, UnidadesModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    ProdutosModule,
+    PedidosModule,
+    UnidadesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
