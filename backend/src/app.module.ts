c58438da4569fe82e59d93f224fe3340fc5e0475
jgfn1 <jgfn1@cin.ufpe.br>
2025-04-22 13:52:25 -0300

    Add Auth, Produtos, Pedidos, and Unidades modules to the backend; update app.module.ts to include new modules in imports.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProdutosModule } from './produtos/produtos.module';
+import { PedidosModule } from './pedidos/pedidos.module';
+import { UnidadesModule } from './unidades/unidades.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProdutosModule, PedidosModule, UnidadesModule],
   controllers: [AppController],
   providers: [AppService],
 })
