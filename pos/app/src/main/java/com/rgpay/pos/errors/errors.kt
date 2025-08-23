6dabeca7a5181ca064b032958bf9769911d18837
José R. A. Figueirôa <josericardoaf@gmail.com>
2025-08-22 22:27:10 -0300

    feat: add errors

---
+++ pos/app/src/main/java/com/rgpay/pos/errors/errors.kt
@@ -0,0 +1,21 @@
+package com.rgpay.pos.errors
+
+// Base class de erro
+open class RgPayError(
+    override val message: String,
+    override val cause: Throwable? = null
+) : Exception(message, cause)
+
+// Device errors
+class DeviceNotFoundError(message: String, cause: Throwable? = null) : RgPayError(message, cause)
+class DeviceNotRegisteredError(message: String, cause: Throwable? = null) :
+    RgPayError(message, cause)
+
+class DeviceAlreadyAssignedError(message: String, cause: Throwable? = null) :
+    RgPayError(message, cause)
+
+class DeviceAlreadyRevokedError(message: String, cause: Throwable? = null) :
+    RgPayError(message, cause)
+
+class DeviceNotAssignedError(message: String, cause: Throwable? = null) : RgPayError(message, cause)
+class ValidationError(message: String, cause: Throwable? = null) : RgPayError(message, cause)
